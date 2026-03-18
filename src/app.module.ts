3d100eb7c0a44ce2a5d6c70f9592d087cd059a23
jovisf <jovisf30@gmail.com>
2026-03-18 14:43:59 -0300

    feat(app): register mqtt module and configure bullmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { FarmsModule } from './farms/farms.module';
-import { PivotsModule } from './pivots/pivots.module';
+import { ConfigModule } from '@/config/config.module';
+import { ConfigService } from '@/config/config.service';
+import { PrismaModule } from '@/prisma/prisma.module';
+import { AuthModule } from '@/auth/auth.module';
+import { UsersModule } from '@/users/users.module';
+import { FarmsModule } from '@/farms/farms.module';
+import { PivotsModule } from '@/pivots/pivots.module';
+import { BullModule } from '@nestjs/bullmq';
+import { MqttModule } from '@/mqtt/mqtt.module';
 
 @Module({
   imports: [
@@ -16,6 +19,17 @@ import { PivotsModule } from './pivots/pivots.module';
     UsersModule,
     FarmsModule,
     PivotsModule,
+    MqttModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.getRedisHost(),
+          port: config.getRedisPort(),
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
